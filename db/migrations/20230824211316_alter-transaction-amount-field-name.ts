77a74267917e3d4ac7fa740c458c124b9ca4a46a
Rodrigo Teixeira Ribeiro da Silva <rodrigoteix1998@gmail.com>
2023-08-25 00:25:56 -0300

    Adding knex db, sqlite and env variables

---
+++ db/migrations/20230824211316_alter-transaction-amount-field-name.ts
@@ -0,0 +1,15 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("transactions", (table) => {
+    table.dropColumn("amout");
+    table.decimal("amount", 10, 2).notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("transactions", (table) => {
+    table.dropColumn("amount");
+    table.decimal("amout", 10, 2).notNullable();
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,31 @@
-import fastify from 'fastify'
+import fastify from "fastify";
+import { knex } from "./database";
+import { randomUUID } from "node:crypto";
 
-const app = fastify()
+const app = fastify();
 
-app.get('/hello', () => {
-  return 'Hello World'
-})
+app.get("/hello", async () => {
+  const transaction = await knex("transactions")
+    .insert({
+      id: randomUUID(),
+      title: "Transacao de teste",
+      amount: 1000,
+    })
+    .returning("*");
+
+  return transaction;
+});
+
+app.get("/hello2", async () => {
+  const transaction = await knex("transactions").select("*");
+
+  return transaction;
+});
 
 app
   .listen({
     port: 3334,
   })
   .then(() => {
-    console.log('HTTP Server Running')
-  })
+    console.log("HTTP Server Running");
+  });
